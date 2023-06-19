d7c5b8e6b96f213d47e368acd818da2c20ecbbfa
Pablo <pablochambi577@hotmail.com>
2023-06-19 16:04:26 -0300

    giftCard Responsive

---
+++ js/detalles-curso-java.js
@@ -0,0 +1,8 @@
+let inscribirse = document.querySelector("#inscribirse");
+let modal = document.querySelector(".modal");
+
+
+inscribirse.addEventListener("click",(e)=>{
+    modal.classList.remove("ocutar");
+});
+
--- js/gifdCard.js
+++ js/gifdCard.js
@@ -4,14 +4,13 @@ let nombreGiftCard = document.querySelector(".nombreDestinatario");
 
 let tamanioFuente = document.getElementsByName("tamanio-fuente");
 
-let fechaHoraGiftCard = document.querySelector(".fechaHoraGift");
-
-let color = document.getElementsByName("color");
+let color = document.getElementsByName("color");  
 
 let monto = document.querySelector("#monto");
 let precio = document.querySelector(".precio");
 
-let ubicacionFechaHora = document.getElementsByName("ubicacion");
+let ubicacionPrecio = document.getElementsByName("ubicacion");//ubicacion precio
+let precioGiftCard = document.querySelector(".precio");
 
 let fondos = document.getElementsByName("fondo");
 let fondoGiftCard = document.querySelector(".conteiner-giftCard");
@@ -26,8 +25,6 @@ color.forEach((item)=>{
         nombreGiftCard.classList.remove("azul","celeste","violeta","naranja","negro");
         nombreGiftCard.classList.add(item.value);
         //console.log(item.value);
-        fechaHoraGiftCard.classList.remove("azul","celeste","violeta","naranja","negro");
-        fechaHoraGiftCard.classList.add(item.value);
 
         giftCardPara.classList.remove("azul","celeste","violeta","naranja","negro");
         giftCardPara.classList.add(item.value);
@@ -45,16 +42,16 @@ tamanioFuente.forEach((item)=>{
 
 monto.addEventListener("keyup",(e)=>{
     if(monto.value==""){
-        precio.innerHTML = "$ 0 .-";
+        precio.innerHTML = "$0.-";
     }else{
-        precio.innerHTML ="$ "+ monto.value + " .-";
+        precio.innerHTML ="$"+ monto.value + ".-";
     }
 });
 
-ubicacionFechaHora.forEach((item)=>{
+ubicacionPrecio.forEach((item)=>{
     item.addEventListener("click", (e)=>{
-        fechaHoraGiftCard.classList.remove("abajoDerecha", "arribaIzquierda", "arribaDerecha");
-        fechaHoraGiftCard.classList.add(item.value);
+        precioGiftCard.classList.remove("abajoDerecha", "arribaIzquierda", "arribaDerecha");
+        precioGiftCard.classList.add(item.value);
     });
 });
 
