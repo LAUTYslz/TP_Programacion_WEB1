3eaf4571ce1fbd4092519c2d204c82283b758c32
Pablo <pablochambi577@hotmail.com>
2023-06-22 11:51:51 -0300

    agregue un popup en detalles de curso

--- js/detalles-curso-java.js
+++ js/detalles-curso-java.js
@@ -1,10 +1,19 @@
-let inscribirse = document.querySelector("#inscribirse");
-let modal = document.querySelector(".modal");
+let inscribirse = document.querySelector("#informacion-curso__inscribirse");
+let modal = document.querySelector(".section-modal");
+
+let iconoCerrar = document.querySelector(".icono_cerrar");
 
 
-inscribirse.addEventListener("click",(e)=>{
-    modal.classList.remove("ocutar");
+inscribirse.addEventListener("click",()=>{
+  
+    modal.classList.remove("ocultar");
 });
+
+iconoCerrar.addEventListener("click", ()=>{
+  modal.classList.add("ocultar");
+});
+
+
 
 $(".acordeon").on("click", ".acordeon-cabecera", function() {
   $(this).toggleClass("active").next().slideToggle();
