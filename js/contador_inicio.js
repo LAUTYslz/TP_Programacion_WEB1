ea7853db43c36080459b510ec7cc1b77c1b1b4fb
lautaro salazar <salazarlautaroalejandro@hotmail.com>
2023-06-30 18:12:04 -0300

    menu fijado: arreglo

---
+++ js/contador_inicio.js
@@ -0,0 +1,4 @@
+let listaCursosI = document.getElementById("lista_cursos");
+let contador_cursoI = document.getElementById("contador");
+let mensajeListaI = document.querySelector("#mensajelista");
+
