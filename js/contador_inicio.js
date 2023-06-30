98a8fac0d96cca3e9f85e7204d9475f817f3dac9
lautaro salazar <salazarlautaroalejandro@hotmail.com>
2023-06-30 20:34:44 -0300

    contador y compra: creacion y modificacion de popup compra y contador

--- js/contador_inicio.js
+++
@@ -1,4 +0,0 @@
-let listaCursosI = document.getElementById("lista_cursos");
-let contador_cursoI = document.getElementById("contador");
-let mensajeListaI = document.querySelector("#mensajelista");
-
--- js/lista de cursos.js
+++ js/lista de cursos.js
@@ -1,12 +1,20 @@
 let listaCursos = document.getElementById("lista_cursos");
 let contador_curso = document.getElementById("contador");
 let mensajeLista = document.querySelector("#mensajeLista");
+let botonesComprar = document.querySelectorAll('.boton_comprar');
+
+
 
 function actualizarListaCursos() {
     if (parseInt(contador_curso.textContent) === 0) {
         mensajeLista.textContent = "Todavía no se obtuvieron cursos";
     } else {
-        mensajeLista.textContent = "Agregar cursos";
+        mensajeLista.textContent ="";
+        cursos.forEach((curso) =>{
+            const nuevoCurso = document.createElement('p');
+            nuevoCurso.textContent=curso;
+            mensajeLista.appendChild(nuevoCurso);
+        });        
     }
 }
 
@@ -22,4 +30,57 @@ contador_curso.addEventListener('click', function() {
 // Actualizar la lista de cursos cuando se carga la página
 window.addEventListener('load', actualizarListaCursos);
 
+let cursos=[];
+let cantidadCursos= 0;
 
+function guardarCursos() {
+    localStorage.setItem("cursos", JSON.stringify(cursos));
+    localStorage.setItem("cantidadCursos", cantidadCursos);
+}
+
+function cargarCursosGuardados() {
+    const cursosGuardados = localStorage.getItem("cursos");
+    const cantidadCursosGuardados = localStorage.getItem("cantidadCursos");
+    
+    if (cursosGuardados) {
+        cursos = JSON.parse(cursosGuardados);
+        cantidadCursos = parseInt(cantidadCursosGuardados);
+        
+        contador_curso.textContent = cantidadCursos.toString();
+        actualizarListaCursos();
+        
+        cursos.forEach((curso) => {
+            const nuevoCurso = document.createElement('p');
+            nuevoCurso.textContent = curso;
+            mensajeLista.appendChild(nuevoCurso);
+        });
+    }
+}
+
+// Actualizar la lista de cursos cuando se carga la página
+window.addEventListener('load', function() {
+    cargarCursosGuardados();
+    actualizarListaCursos();
+});
+
+botonesComprar.forEach((boton)=>{
+    boton.addEventListener("click",()=>{
+        const curso = boton.parentElement;
+        const titulo = curso.querySelector('h3').textContent;
+
+        cursos.push(titulo);
+        cantidadCursos++;
+
+        contador_curso.textContent=cantidadCursos.toString();
+        actualizarListaCursos();
+
+        mensajeLista.textContent="";
+        cursos.forEach((curso) =>{
+            const nuevoCurso = document.createElement('p');
+            nuevoCurso.textContent=curso;
+            mensajeLista.appendChild(nuevoCurso);
+        });
+
+        guardarCursos();
+    })
+})
